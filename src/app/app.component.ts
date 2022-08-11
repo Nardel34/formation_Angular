cb724ffb7165ffa611213ada6c427c80e1fffa6e
Victor Nardella <victor.nardel@gmail.com>
2022-08-11 17:20:42 +0200

    Avancer du cours + ajout de Materialize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 @Component({
   selector: 'app-root',
-  template: `<h1>Bonjour {{pokemons_list[1]}}</h1>`
+  templateUrl: 'app.component.html'
 })
 export class AppComponent implements OnInit{
-  pokemons_list = ['Bulbizarre', 'Salamèche', 'Carapuce'];
+  pokemons_list: Pokemon[] = POKEMONS;
+  pokemonSelected : Pokemon|undefined;
   
   ngOnInit(): void{
     console.table(this.pokemons_list);
+  }
+
+  selectPokemon(pokemonId: string){
+    
+    //const index: number = +(event.target as HTMLInputElement).value; // number(...) ou +
+    const i = +pokemonId;
+    const pokemon: Pokemon|undefined = this.pokemons_list.find(pokemon => pokemon.id == +pokemonId);
+
+    if(pokemon){
+      console.log(`Vous avez cliqué sur le pokemon ${pokemon.name}`)
+      this.pokemonSelected = pokemon;
+    } else {
+      console.log(`Vous avez demandé un Pokémon qui n'existe pas !`)
+      this.pokemonSelected = pokemon;
+    }
   }
 }
