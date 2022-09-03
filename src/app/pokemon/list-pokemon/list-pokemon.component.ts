fc784a11cd495caffb384ea593ce55a030e99fd4
Victor Nardella <victor.nardel@gmail.com>
2022-09-03 16:29:26 +0200

    Restructuration du projet

--- src/app/pokemon/list-pokemon/list-pokemon.component.ts
+++ src/app/pokemon/list-pokemon/list-pokemon.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { POKEMONS } from '../mock-pokemon-list';
 import { Pokemon } from '../pokemon';
-
-
 @Component({
   selector: 'app-list-pokemon',
   templateUrl: './list-pokemon.component.html',
 })
+
 export class ListPokemonComponent {
 
   pokemons_list: Pokemon[] = POKEMONS;
